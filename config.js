dbcf0f857f989ac8e81152616dac52afdbf22f8c
olav <olav@bjorkoy.com>
2012-11-13 19:10:56 +0100

    Stylezh.

--- config.js
+++ config.js
@@ -7,12 +7,13 @@ exports.config = {
         "js/vendor/json2.js",
         "js/vendor/underscore.js",
         "js/vendor/jquery.js",
-        "js/script.js"
+        "js/main.js"
       ]
     },
 
     css: {
       all: [
+        "css/reset.css",
         "css/style.css"
       ]
     }
