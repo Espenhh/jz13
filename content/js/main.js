67496586f9cf6e88d6651552095f047065b94d24
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2013-01-29 18:49:14 +0100

    Fikser riktige partnere

--- content/js/main.js
+++ content/js/main.js
@@ -57,34 +57,40 @@ $(function() {
 });
 
 jz.data.partners = [
-    ["Systek", "systek_2012.jpg", "http://www.systek.no/"],
-    ["Arktekk", "arktekk_2012.jpg", "http://www.arktekk.no/"],
     ["NETS", "nets_2012.jpg", "http://www.nets.no/"],
-    ["Marcello", "marcello_2012.jpg", "http://www.marcello.no/"],
-    ["Evry", "edb_int_2012.jpg", "http://www.evry.com/"],
-    ["Visme", "visma_2012.jpg", "http://www.visma.no/"],
-    ["Bekk", "bekk_2012.jpg", "http://www.bekk.no/"],
-    ["Computas", "computas_2012.jpg", "http://www.computas.no/"],
-    ["jPro", "jpro_2012.jpg", "http://www.jpro.no/"],
-    ["Computas", "nith_2012.jpg", "http://nith.no/"],
-    ["Steria", "steria_2012.jpg", "http://www.steria.no/"],
-    ["Kantega", "kantega_2012.jpg", "http://www.kantega.no/"],
-    ["Iterate", "iterate_2012.jpg", "http://www.iterate.no/"],
-    ["Conax", "conax_2012.jpg", "http://www.conax.no/"],
-    ["Microsoft", "microsoft_2012.jpg", "http://www.microsoft.no/"],
-    ["KnowIT", "knowit_2012.jpg", "http://www.knowit.no/"],
-    ["Mesan", "mesan_2012.jpg", "http://www.mesan.no/"],
-    ["Norgesgruppen", "norgesgruppen_2012.jpg", "http://www.norgesgruppen.no/"],
-    ["Bouvet", "bouvet_2012.jpg", "http://www.bouvet.no/"],
-    ["Kodemaker", "kodemaker_2012.jpg", "http://www.kodemaker.no/"],
     ["Accenture", "accenture_2012.jpg", "http://www.accenture.no/"],
+    ["Arktekk", "arktekk_2012.jpg", "http://www.arktekk.no/"],
+    ["Bekk", "bekk_2012.jpg", "http://www.bekk.no/"],
     ["Cap Gemini", "capgemini_2012.jpg", "http://www.capgemini.no/"],
-    ["Cisco", "cisco_2012.jpg", "http://www.cisco.com/"],
     ["Ciber", "ciber_2012.jpg", "http://www.ciber.no/"],
-    ["Programutviling", "programutvikling_2012.jpg", "http://www.put.no/"],
+    ["Cisco", "cisco_2012.jpg", "http://www.cisco.com/"],
+    ["Computas", "nith_2012.jpg", "http://nith.no/"],
+    ["Conax", "conax_2012.jpg", "http://www.conax.no/"],
+    ["Conduct", "conduct_2012.jpg", "http://www.conduct.no"], //TODO
+    ["Finn.no", "finn_2012.jpg", "http://www.finn.no"],
+    ["Inmeta", "inmeta_2012.jpg", "http://www.inmetacrayon.no/"], //TODO
     ["Itera", "itera_2012.jpg", "http://www.iteraconsulting.no/"],
-    ["Webstep", "wepstep_2012.jpg", "http://www.webstep.no/"],
+    ["jPro", "jpro_2012.jpg", "http://www.jpro.no/"],
+    ["Kantega", "kantega_2012.jpg", "http://www.kantega.no/"],
+    ["KnowIT", "knowit_2012.jpg", "http://www.knowit.no/"],
+    ["Kodemaker", "kodemaker_2012.jpg", "http://www.kodemaker.no/"],
+    ["Marcello", "marcello_2012.jpg", "http://www.marcello.no/"],
+    ["Mesan", "mesan_2012.jpg", "http://www.mesan.no/"],
+    ["Microsoft", "microsoft_2012.jpg", "http://www.microsoft.no/"],
     ["Miles", "miles_2012.jpg", "http://www.miles.no/"],
-    ["Finn.no", "finn_2012.jpg", "http://www.finn.no"]
+    ["NITH", "nith_2012.jpg", "http://www.nith.no/"], //TODO
+    ["Programutvikling", "programutvikling_2012.jpg", "http://www.put.no/"],
+    ["Skatteetaten", "skatteetaten_2012.jpg", "http://www.skatteetaten.no/"], //TODO
+    ["Steria", "steria_2012.jpg", "http://www.steria.no/"],
+    ["Systek", "systek_2012.jpg", "http://www.systek.no/"],
+    ["ViaNett", "vianett_2012.jpg", "http://www.vianett.no/"],
+    ["Visma", "visma_2012.jpg", "http://www.visma.no/"],
+    ["Webstep", "wepstep_2012.jpg", "http://www.webstep.no/"]
+
+    //["Evry", "edb_int_2012.jpg", "http://www.evry.com/"],
+    //["Computas", "computas_2012.jpg", "http://www.computas.no/"],
+    //["Iterate", "iterate_2012.jpg", "http://www.iterate.no/"],
+    //["Norgesgruppen", "norgesgruppen_2012.jpg", "http://www.norgesgruppen.no/"],
+    //["Bouvet", "bouvet_2012.jpg", "http://www.bouvet.no/"],
 ];
 
