76db12869d85b1a09bc847ded33bd9d7f87618b8
olav <olav@bjorkoy.com>
2013-08-14 17:50:12 +0200

    Mer mobiltilpassing

--- content/assets/js/date.js
+++ content/assets/js/date.js
@@ -2,7 +2,7 @@ jz.date.parse = function(date) {
     var    m = /(\d\d\d\d)-(\d\d)-(\d\d).(\d\d):(\d\d):(\d\d)/.exec(date);
     if(!m) m = /(\d\d\d\d)-(\d\d)-(\d\d)/.exec(date);
     if(!m) return "";
-    var hourWithTimeZoneFixed = parseInt(m[4]) + 2; // Olav: ok? :P
+    var hourWithTimeZoneFixed = "" + (parseInt(m[4], 10) + 2);
     return { year: m[1], month: m[2], day: m[3], hour: hourWithTimeZoneFixed, min: m[5], sec: m[6] };
 };
 
@@ -13,16 +13,21 @@ jz.date.date = function(date) {
 
 jz.date.time = function(date) {
     var m = jz.date.parse(date);
-    return !m ? "" : m.hour + ":" + m.min;
+    var h = m.hour.length === 1 ? "0" + m.hour : m.hour;
+    return !m ? "" : h + ":" + m.min;
+};
+
+jz.date.weekday = function(date) {
+    var d  = (new Date(date));
+    var ds = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    return ds[d.getDay()];
 };
 
 jz.date.day = function(date) {
     var d  = (new Date(date));
-    var ds = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
     var ms = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-    var day   = ds[d.getDay()];
     var month = ms[d.getMonth()];
-    return day + ', ' + d.getDate() + ' ' + month + ' ' + d.getFullYear();
+    return d.getDate() + ' ' + month + ' ' + d.getFullYear();
 };
 
 jz.date.duration = function(start, stop) {
